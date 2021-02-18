f95d5212a4289707ec715183ba02d61dc0152852
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2021-02-18 14:35:13 +0000

    add: bootstrap dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+if (typeof window !== "undefined") {
+  require('bootstrap');
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
